025d9a68d834889a016a7fc6fc3ec35d3d7f3ec3
Scott Carroll <scott_carroll19@yahoo.com>
2018-07-26 16:55:17 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = 1;
+    var value = 5;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -181,8 +181,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 987) self.won = true;
+          // The mighty winning tile (4935, in this case)
+          if (merged.value === 4935) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
